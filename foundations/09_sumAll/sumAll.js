1d4994dd762c31bac9405f3b662fdaa8b15ba100
JackWilliamVass <jackjackv05@gmail.com>
2026-06-15 20:13:10 +0100

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!(Number.isInteger(x))
+        || !(Number.isInteger(y))
+        || x <= 0
+        || y <= 0) 
+        {return "ERROR";}
+    const sumAllLocal = (a, b) => {
+        let total = 0;
+        for (let i = a; a <= b; a++) total += a;
+        return total;
+    }
+    return (x > y) ? sumAllLocal(y, x) : sumAllLocal(x, y); 
 };
 
 // Do not edit below this line
