da63d00e35fc5a1661599c8b42cd13e0cdf9bc26
JackWilliamVass <jackjackv05@gmail.com>
2026-06-15 19:57:08 +0100

    Finish 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter((item) => {
+        return !args.includes(item)
+    });
 };
-
+removeFromArray([1,2,3,4,5],1,2);
 // Do not edit below this line
 module.exports = removeFromArray;
