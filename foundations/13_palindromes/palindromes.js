6f686020b780a8860e417d3946bd12b63d5e0166
JackWilliamVass <jackjackv05@gmail.com>
2026-06-26 19:34:33 +0100

    Finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const alphanumerical = "abcdefghijkmlnopqrstuvwxyz0123456789";
 
+const palindromes = function (string) {
+    let stringArr = string.toLowerCase().split('');
+    stringArr = stringArr.filter(char => alphanumerical.includes(char));
+    return stringArr.toString() === (stringArr.reverse().toString());
 };
 
 // Do not edit below this line
