b7e93d2fdebc82e3bbf5dc091bd33fad440c0df0
JackWilliamVass <jackjackv05@gmail.com>
2026-06-27 14:35:12 +0100

    Finish 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(b) {
+    let num1 = 1;
+    let num2 = 1;
 
+    let a = Number(b);
+
+    if (a === 1 || a === 2) {return num1;}
+    if (a === 0) {return 0;}
+    else if (a < 0) {return "OOPS";}
+
+    for (let i = 2; i < a; i++) {
+        const next = num1 + num2;
+
+        num1 = num2;
+        num2 = next;
+    }
+    return num2;
 };
 
 // Do not edit below this line
