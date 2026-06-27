15103f49047281b6bedfcdc38208105e4c9d4c02
JackWilliamVass <jackjackv05@gmail.com>
2026-06-27 17:11:17 +0100

    Finish 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const oldAge = ("yearOfDeath" in oldest) ? oldest.yearOfDeath - oldest.yearOfBirth :
+                        new Date().getFullYear() - oldest.yearOfBirth
+        const newAge = ("yearOfDeath" in person) ? person.yearOfDeath - person.yearOfBirth :
+                        new Date().getFullYear() - person.yearOfBirth
 
+        return (newAge > oldAge) ? person : oldest;
+    });
 };
 
 // Do not edit below this line
